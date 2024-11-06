874a18d798c7c57a214192e6cbdc49623189e1a5
frectonz <fraol0912@gmail.com>
2024-11-06 12:01:15 +0300

    feat: create a web socker wrapper service

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use calculator::Calculator;
 use clap::Parser;
 use color_eyre::eyre::Context;
-use futures::{future, FutureExt, StreamExt, TryStreamExt};
+use futures::{future, StreamExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Message;
 use tower::Service;
+use websocket::WebSocketAdapter;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -45,18 +47,25 @@ async fn start(address: &str) -> color_eyre::Result<()> {
         .context("failed to create tcp listener")?;
     tracing::info!("listening on: {}", address);
 
+    let svc = WebSocketAdapter::new(Calculator);
+
     loop {
         match listener.accept().await {
             Ok((stream, peer_addr)) => {
-                tracing::info!("accepted connection to {peer_addr}");
-                match accept_connection(stream).await {
-                    Ok(()) => {
-                        tracing::info!("web socket connection existed");
-                    }
-                    Err(e) => {
-                        tracing::error!("failed to handle web socket connection: {e}");
-                    }
-                };
+                let svc = svc.clone();
+
+                tokio::spawn(async move {
+                    tracing::info!("accepting connection to {peer_addr}");
+
+                    match accept_connection(stream, svc).await {
+                        Ok(()) => {
+                            tracing::info!("web socket connect exited");
+                        }
+                        Err(e) => {
+                            tracing::error!("failed to handle web socket connection: {e}");
+                        }
+                    };
+                });
             }
             Err(e) => {
                 tracing::error!("failed to accept tcp connection: {e}");
@@ -90,23 +99,101 @@ struct OpResult {
     result: u8,
 }
 
-async fn accept_connection(stream: TcpStream) -> Result<(), tokio_tungstenite::tungstenite::Error> {
+async fn accept_connection<S>(
+    stream: TcpStream,
+    mut svc: S,
+) -> Result<(), tokio_tungstenite::tungstenite::Error>
+where
+    S: Service<Message, Response = Message, Error = tokio_tungstenite::tungstenite::Error>,
+{
+    tracing::info!("accepted connection");
+
     let ws_stream = tokio_tungstenite::accept_async(stream).await?;
     tracing::info!("new web socket connection established");
 
     let (write, read) = ws_stream.split();
+    read
+        .try_filter(|msg| future::ready(msg.is_text()))
+        .and_then(|msg| svc.call(msg))
+        .forward(write)
+        .await?;
 
+    Ok(())
+}
+
+mod calculator {
+    use std::task::{Context, Poll};
+
+    use futures::future;
+    use tower::Service;
+
+    use crate::{ApiRequest, ApiResponse, OpResult, TwoNumsRequest};
+
+    #[derive(Clone)]
+    pub struct Calculator;
+
+    impl Service<ApiRequest> for Calculator {
+        type Response = ApiResponse;
+        type Error = tower::BoxError;
+        type Future = future::Ready<Result<Self::Response, Self::Error>>;
+
+        fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, request: ApiRequest) -> Self::Future {
+            future::ok(match request {
+                ApiRequest::Add(TwoNumsRequest { x, y }) => ApiResponse::AddResult(OpResult {
+                    result: x.wrapping_add(y),
+                }),
+                ApiRequest::Sub(TwoNumsRequest { x, y }) => ApiResponse::SubResult(OpResult {
+                    result: x.wrapping_sub(y),
+                }),
+            })
+        }
+    }
+}
+
+mod websocket {
+    use std::task::{Context, Poll};
+
+    use futures::{future, FutureExt};
+    use serde::Serialize;
     use tokio_tungstenite::tungstenite::Message;
+    use tower::Service;
 
-    let mut svc = Calculator;
+    use crate::ApiRequest;
 
-    let _ = read
-        .and_then(|msg| {
-            if let Message::Text(body) = msg {
+    #[derive(Clone)]
+    pub struct WebSocketAdapter<S>(S);
+
+    impl<S> WebSocketAdapter<S> {
+        pub fn new(inner: S) -> Self {
+            Self(inner)
+        }
+    }
+
+    impl<S> Service<Message> for WebSocketAdapter<S>
+    where
+        S: Service<ApiRequest>,
+        S::Response: Serialize,
+        S::Future: Send + 'static,
+    {
+        type Response = Message;
+        type Error = tokio_tungstenite::tungstenite::Error;
+        type Future = future::BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+        fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, request: Message) -> Self::Future {
+            if let Message::Text(body) = request {
                 let req = serde_json::from_str::<ApiRequest>(&body);
                 match req {
                     Ok(req) => {
-                        let res = svc
+                        let res = self
+                            .0
                             .call(req)
                             .map(|body| {
                                 Ok(match body {
@@ -126,44 +213,9 @@ async fn accept_connection(stream: TcpStream) -> Result<(), tokio_tungstenite::t
                     }
                 }
             } else {
-                tracing::error!("received a non-text message");
+                tracing::error!("received a non-text message: {request:?}");
                 future::ok(Message::text("i only understand text messages")).boxed()
             }
-        })
-        .forward(write)
-        .await;
-
-    Ok(())
-}
-
-mod calculator {
-    use std::task::{Context, Poll};
-
-    use futures::future;
-    use tower::Service;
-
-    use crate::{ApiRequest, ApiResponse, OpResult, TwoNumsRequest};
-
-    pub struct Calculator;
-
-    impl Service<ApiRequest> for Calculator {
-        type Response = ApiResponse;
-        type Error = std::convert::Infallible;
-        type Future = future::Ready<Result<ApiResponse, std::convert::Infallible>>;
-
-        fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-            Poll::Ready(Ok(()))
-        }
-
-        fn call(&mut self, request: ApiRequest) -> Self::Future {
-            future::ok(match request {
-                ApiRequest::Add(TwoNumsRequest { x, y }) => ApiResponse::AddResult(OpResult {
-                    result: x.wrapping_add(y),
-                }),
-                ApiRequest::Sub(TwoNumsRequest { x, y }) => ApiResponse::SubResult(OpResult {
-                    result: x.wrapping_sub(y),
-                }),
-            })
         }
     }
 }
