453e44beb24a96f63ba3ba8ae1016878c8ee14f4
frectonz <fraol0912@gmail.com>
2024-11-13 15:49:23 +0300

    feat: setup nix ci

--- flake.nix
+++ flake.nix
@@ -47,6 +47,37 @@
         };
       });
 
+      packages = forAllSystems (pkgs:
+        let
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+
+          pname = "jabroni";
+          version = "0.1.0";
+        in rec {
+          default = rustPlatform.buildRustPackage {
+            inherit pname version;
+            src = ./.;
+            buildInputs = [ pkgs.openssl ];
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          image = pkgs.dockerTools.buildLayeredImage {
+            name = pname;
+            tag = "latest";
+            created = "now";
+            config.Cmd = [ "${default}/bin/jabroni" ];
+          };
+
+          deploy = pkgs.writeShellScriptBin "deploy" ''
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:${image} docker://docker.io/frectonz/jabroni:${version} --dest-creds="frectonz:$ACCESS_TOKEN"
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker://docker.io/frectonz/jabroni:${version} docker://docker.io/frectonz/jabroni:latest --dest-creds="frectonz:$ACCESS_TOKEN"
+          '';
+        });
+
       formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
     };
 }
